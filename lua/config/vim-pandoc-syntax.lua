858411a4c62a67bda9d920918d9c170c20cf0ab7
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:56:46 -0400

    Configured enabled and disabled settings for pandoc

--- lua/config/vim-pandoc-syntax.lua
+++ lua/config/vim-pandoc-syntax.lua
@@ -1,1 +1,8 @@
--- vim.g.
+vim.g['pandoc#filetypes#handled'] = {'pandoc', 'markdown'} -- When to activate pandoc
+vim.g['pandoc#modules#disabled'] = {'folding', 'toc', 'templates', 'yaml'} -- What pandoc modules to disable
+vim.g['pandoc#biblio#bibs'] = {'/home/src/Knowledgebase/Zettelkasten/zettel.bib', '/home/src/Projects/C3T/citations.bib'} -- Sources for bibliography citations
+vim.g['pandoc#completion#bib#use_preview'] = 1 -- Display additional bibliographic information in a preview window
+vim.g['pandoc#completion#bib#mode'] = 'citeproc' -- What backend to use
+vim.g['pandoc#hypertext#split_open_cmd'] = 'vsplit' -- How to split-open files
+vim.g['pandoc#hypertext#autosave_on_edit_open_link'] = 1 -- Automatically save current file when following link in same window
+vim.g['pandoc#syntax#codeblocks#embeds#langs'] = {'julia', 'python', 'haskell', 'r', 'markdown', 'lua', 'make', 'sh'} -- Syntax highlighting for fenced markdown code blocks 
