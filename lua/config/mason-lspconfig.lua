0ebd663739dcc8eb7a3ccc0dc38e92dbcf661cb5
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:58:13 -0400

    Updated defaults for what LSPs must be installed

--- lua/config/mason-lspconfig.lua
+++ lua/config/mason-lspconfig.lua
@@ -1,1 +1,4 @@
-require("mason-lspconfig").setup()
+-- NOTE: For some reason, you may need to open neovim once or twice for r_language_server to properly download
+require("mason-lspconfig").setup({
+    ensure_installed = { "sumneko_lua", "julials", "marksman", "r_language_server", "taplo", "pylsp" }
+})
