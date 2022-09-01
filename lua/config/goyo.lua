4a6b02ff1b01821b854600dfbb818f05353e01bd
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:53:57 -0400

    Added Goyo for distraction free writing

---
+++ lua/config/goyo.lua
@@ -0,0 +1,32 @@
+vim.g.goyo_width = '85%' -- Default width
+vim.g.goyo_height = '90%' -- Default height
+
+vim.cmd [[
+function! s:goyo_enter()
+  " Hides mode from showing
+  set noshowmode 
+
+  " Hides the sign column
+  :set scl=no 
+
+  " Hides lualine
+  lua require"lualine".hide() 
+
+  " ...
+endfunction
+function! s:goyo_leave()
+  " Resets syntax highlighting (workaround for goyo bug)
+  syntax off 
+  syntax on 
+
+  " Brings mode back
+  set showmode 
+
+  " Shows lualine again
+  lua require"lualine".hide({unhide=true})
+
+  " ...
+endfunction
+autocmd! User GoyoEnter nested call <SID>goyo_enter()
+autocmd! User GoyoLeave nested call <SID>goyo_leave()
+]]
