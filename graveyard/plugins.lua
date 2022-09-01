f2728248ba77ef18e7bc99f4b31d2c57384dedaa
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:40:07 -0400

    Removed packages and updated notes on why it was removed

--- graveyard/plugins.lua
+++ graveyard/plugins.lua
@@ -1,10 +1,14 @@
-  --[[
+-- A minimal, configurable, neovim style tabline.
+-- REMOVAL REASON: Although the tabs looked great and were nice to have, its inability to save names of tab groups across sessions led me to deprecate this plugin in favor of Taboo.vim.
+-- use {'nanozuki/tabby.nvim', config = [[require('config.tabby')]]}
 
-  -- NAME: Tabby
-  -- DESC: A minimal, configurable, neovim style tabline.
 
-  -- REMOVAL REASON: Although the tabs looked great and were nice to have, its inability to save names of tab groups across sessions led me to deprecate this plugin in favor of Taboo.vim.
+-- A theme for vim and neovim [CONFIGURED]
+-- REMOVAL REASON: I enjoyed this colorscheme but gruvbox.nvim called me back to its warm embrace
+-- use { 'EdenEast/nightfox.nvim', config = [[require('config.nightfox')]] }
 
-  --]]
 
-  -- use {'nanozuki/tabby.nvim', config = [[require('config.tabby')]]}
+-- Clean and elegant distraction-free writing for NeoVim
+-- REMOVAL REASON: Although I really liked this, it is just not mature enough and I found it cumbersome to configure when it didn't work just yet out of the box - switching back to Goyo
+-- use { 'Pocco81/true-zen.nvim', config = [[require('config.true-zen')]] }
+
--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,20 +1,25 @@
+-- Helper function for setting Obsession status in lualine; remove if not using vim-obsession
+local function obsession_status()
+  return vim.fn.ObsessionStatus('', '')
+end
+
 require('lualine').setup {
   options = {
-    icons_enabled = true,
-    theme = 'gruvbox',
+    icons_enabled = false,
+    theme = 'codedark',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
-    globalstatus = false,
+    globalstatus = true,
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = {obsession_status, 'filename'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {'filetype'}
   },
   inactive_sections = {
     lualine_a = {},
