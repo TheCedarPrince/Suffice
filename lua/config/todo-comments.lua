99dd0b5525b482ff83c9d797c3d5fb46cbeffd7a
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:57:47 -0400

    Updated highlight behavior, line length, and colors

--- lua/config/todo-comments.lua
+++ lua/config/todo-comments.lua
@@ -12,8 +12,8 @@ require("todo-comments").setup(
       },
       TODO = { icon = " ", color = "info" },
       HACK = { icon = " ", color = "warning" },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      WARN = { icon = " ", color = "error", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", color = "perf", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
       NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     },
     merge_keywords = true, -- when true, custom keywords will be merged with the defaults
@@ -22,12 +22,12 @@ require("todo-comments").setup(
     -- * keyword: highlights of the keyword
     -- * after: highlights after the keyword (todo text)
     highlight = {
-      before = "fg", -- "fg" or "bg" or empty
-      keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+      before = "", -- "fg" or "bg" or empty
+      keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
       after = "fg", -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+      pattern = [[(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
-      max_line_len = 400, -- ignore lines longer than this
+      max_line_len = 2000, -- ignore lines longer than this
       exclude = {}, -- list of file types to exclude highlighting
     },
     -- list of named colors where we try to extract the guifg from the
@@ -37,6 +37,7 @@ require("todo-comments").setup(
       warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
       info = { "DiagnosticInfo", "#2563EB" },
       hint = { "DiagnosticHint", "#10B981" },
+      perf = { "Number", "#B16286" },
       default = { "Identifier", "#7C3AED" },
     },
     search = {
