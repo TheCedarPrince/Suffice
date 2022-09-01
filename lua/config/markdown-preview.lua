a65b7d2628818b6ca9f6e0ea687e264ee114a4c4
Jacob Zelko <jacobszelko@gmail.com>
2022-09-01 10:55:57 -0400

    Simple configurations for previewing markdown in browser

---
+++ lua/config/markdown-preview.lua
@@ -0,0 +1,6 @@
+vim.g.mkdp_auto_close = 0 -- Auto close current preview window when change from markdown buffer to another buffer
+vim.g.mkdp_refresh_slow = 0 -- Refresh preview while editing
+
+vim.g.mkdp_page_title = '${name}' -- Preview page title
+
+vim.g.mkdp_theme = 'dark' -- Set default theme (dark or light)
