7bef2ed1d8e115bd2a0141e2a7e06a46c23fc6e4
Jacob Zelko <jacobszelko@gmail.com>
2022-09-04 21:17:53 -0400

    Modified to make the signcolumn match the theme color

--- lua/config/gruvbox.lua
+++ lua/config/gruvbox.lua
@@ -1,3 +1,5 @@
+local colors = require('gruvbox.palette')
+
 -- setup must be called before loading the colorscheme
 -- Default options:
 require("gruvbox").setup({
@@ -12,8 +14,17 @@ require("gruvbox").setup({
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
   contrast = "hard", -- can be "hard", "soft" or empty string
-  overrides = {},
+  overrides = {
+    GruvboxRedSign = { fg = colors.red, bg = colors.dark0_hard, reverse = false },
+    GruvboxGreenSign = { fg = colors.green, bg = colors.dark0_hard, reverse = false },
+    GruvboxYellowSign = { fg = colors.yellow, bg = colors.dark0_hard, reverse = false },
+    GruvboxBlueSign = { fg = colors.blue, bg = colors.dark0_hard, reverse = false },
+    GruvboxPurpleSign = { fg = colors.purple, bg = colors.dark0_hard, reverse = false },
+    GruvboxAquaSign = { fg = colors.aqua, bg = colors.dark0_hard, reverse = false },
+    GruvboxOrangeSign = { fg = colors.orange, bg = colors.dark0_hard, reverse = false },
+  }
 })
 
-vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox")
+vim.o.background = 'dark'
+vim.cmd('colorscheme gruvbox')
+vim.cmd([[highlight clear SignColumn]])
