5456f9e41a162632a3de4330e7c7450f2daa405a
TheCedarPrince <jacobszelko@gmail.com>
2022-09-30 14:43:57 -0400

    Configuring folds for LaTeX

--- lua/config/vimtex.lua
+++ lua/config/vimtex.lua
@@ -1,3 +1,4 @@
 vim.g.vimtex_view_general_viewer = 'okular'
 vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
 vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_fold_enabled = 1
