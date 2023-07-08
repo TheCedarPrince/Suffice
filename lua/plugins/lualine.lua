e0f785587ef079fae964793a1ba99f71cf904790
TheCedarPrince <jacobszelko@gmail.com>
2023-07-07 20:04:55 -0400

    Configured lualine for lazy.nvim

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  lazy = false,
+  config = function()
+
+  -- Helper function for setting Obsession status in lualine; remove if not using vim-obsession
+local function obsession_status()
+  return vim.fn.ObsessionStatus('', '')
+end
+
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = {obsession_status, 'filename'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {'progress'},
+    lualine_z = {'filetype'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+  end
+}
