36eb2068b05a79a464588d561ce111b94a71606d
TheCedarPrince <jacobszelko@gmail.com>
2023-07-15 18:18:37 -0400

    Updated Anki decks being used

--- lua/plugins/anki.lua
+++ lua/plugins/anki.lua
@@ -6,6 +6,7 @@ return {
     require("anki").setup({
       tex_support = false,
       models = {
+        ["Reader"] = "Incremental Reading::General",
         ["Math Questions"] = "Mathematics Exercises",
         ["Math Definitions"] = "Mathematics",
         ["Math Definition Properties"] = "Mathematics"
