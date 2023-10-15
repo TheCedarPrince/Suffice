18d25cd01dabffbc4956381f0b71bdb095421d2b
TheCedarPrince <jacobszelko@gmail.com>
2023-10-14 22:47:03 -0400

    Switched live rendering to zathura

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,8 +2,8 @@ return {
   "lervag/vimtex",
   lazy = false,
   config = function()
-    vim.g.vimtex_view_general_viewer = 'okular'
-    vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+    vim.g.vimtex_view_general_viewer = 'zathura'
+    -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
     vim.g.vimtex_compiler_method = 'latexmk'
     vim.g.vimtex_fold_enabled = 1
   end
