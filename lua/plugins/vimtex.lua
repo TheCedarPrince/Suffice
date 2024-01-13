6323a51c1b502787175c3c5f576093fde49ac80e
TheCedarPrince <jacobszelko@gmail.com>
2024-01-13 15:49:15 -0500

    Tweaking for subfiles

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,6 +5,16 @@ return {
     vim.g.vimtex_view_general_viewer = 'zathura'
     -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-bibtex',
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode'
+      }
+    }
     vim.g.vimtex_fold_enabled = 1
   end
 }
