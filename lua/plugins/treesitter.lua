b1851c1cbaf1ea271c1d53472099d331e682d696
TheCedarPrince <jacobszelko@gmail.com>
2026-04-25 19:49:45 -0400

    Added treesitter dependencies

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     dependencies = {
       "folke/which-key.nvim",
-      "nvim-treesitter/nvim-treesitter-textobjects",
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
       {
         "abecodes/tabout.nvim",
         opts = {
@@ -14,34 +15,30 @@ return {
         },
       },
     },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "yaml", "julia" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection    = "<M-w>",
-            scope_incremental = "<CR>",
-            node_incremental  = "<Tab>",
-            node_decremental  = "<S-Tab>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable    = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-            },
-          },
-        },
+    main = "nvim-treesitter",  -- changed from nvim-treesitter.configs
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    init = function()
+      -- ensure_installed is now done via the install API
+      local installed = require("nvim-treesitter.config").get_installed()
+      local wanted = { "markdown", "yaml", "julia" }
+      local to_install = vim.tbl_filter(function(p)
+        return not vim.tbl_contains(installed, p)
+      end, wanted)
+      if #to_install > 0 then
+        require("nvim-treesitter").install(to_install)
+      end
+
+      -- incremental_selection is now a built-in Neovim feature
+      vim.keymap.set("n", "<M-w>", ":lua vim.treesitter.get_node():range()<CR>")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
